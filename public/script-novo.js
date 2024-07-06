399cbc0f754fe3475a8de29f262a4a06a12c84e1
Jonathan Christian Dos Santos Marques <jofuturo123@gmail.com>
2024-07-06 06:00:10 +0000

    update-06-07-2024-01

--- public/script-novo.js
+++
@@ -1,9 +0,0 @@
-function openUniqueOverlay(title, content) {
-    document.getElementById('unique-overlay-title').innerText = title;
-    document.getElementById('unique-overlay-content').innerText = content;
-    document.getElementById('unique-overlay').classList.add('show');
-}
-
-function closeUniqueOverlay() {
-    document.getElementById('unique-overlay').classList.remove('show');
-}
